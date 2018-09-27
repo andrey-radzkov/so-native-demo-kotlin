2ae62afecac3b1744f8a58fdfa72fcff5fcac7ad
andrey.radzkov <andrey.radzkov@gmail.com>
2018-09-27 15:09:48 +0300

    go by url

--- app/src/main/java/com/example/andrey_radzkov/model/BlogListViewAdapter.kt
+++ app/src/main/java/com/example/andrey_radzkov/model/BlogListViewAdapter.kt
@@ -1,6 +1,9 @@
 package com.example.andrey_radzkov.model
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.support.v4.content.ContextCompat.startActivity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,15 +11,15 @@ import android.widget.BaseAdapter
 import com.example.andrey_radzkov.R
 
 
-class BlogListViewAdapter(internal var c: Context, internal var tvShows: List<Article>) : BaseAdapter() {
+class BlogListViewAdapter(internal var c: Context, internal var articles: List<Article>) : BaseAdapter() {
     internal var inflater: LayoutInflater? = null
 
     override fun getCount(): Int {
-        return tvShows.size
+        return articles.size
     }
 
     override fun getItem(position: Int): Any {
-        return tvShows[position]
+        return articles[position]
     }
 
     override fun getItemId(position: Int): Long {
@@ -36,9 +39,14 @@ class BlogListViewAdapter(internal var c: Context, internal var tvShows: List<Ar
 
         //BIND DATA
         val holder = ArticleView(convertView!!)
-        holder.nameTxt.text = tvShows[position].title
-        holder.shortDescription.text = tvShows[position].shortText
-        downloadImage(c, tvShows[position].image, holder.img)
+        holder.nameTxt.text = articles[position].title
+        holder.shortDescription.text = articles[position].shortText
+        downloadImage(c, articles[position].image, holder.img)
+
+        convertView.setOnClickListener(View.OnClickListener {
+            val browse = Intent(Intent.ACTION_VIEW, Uri.parse(articles[position].url))
+            startActivity(c, browse, null)
+        })
 
         return convertView
     }
