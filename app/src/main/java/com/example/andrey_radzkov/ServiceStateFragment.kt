0e2724c0a94fabe7de7687b521a312416104738c
andrey.radzkov <andrey.radzkov@gmail.com>
2018-10-02 13:57:06 +0300

    switch background

--- app/src/main/java/com/example/andrey_radzkov/ServiceStateFragment.kt
+++ app/src/main/java/com/example/andrey_radzkov/ServiceStateFragment.kt
@@ -1,11 +1,13 @@
 package com.example.andrey_radzkov
 
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.RelativeLayout
 import android.widget.TextView
 
 
@@ -14,21 +16,35 @@ import android.widget.TextView
  */
 class ServiceStateFragment : Fragment() {
     lateinit var buttonSwitchMode: Button
+    lateinit var buttonSwitchBackground: Button
     lateinit var buttonLogin: Button
     lateinit var buttonSlm: Button
     lateinit var header: TextView
+    lateinit var layout: RelativeLayout
+    var isBlack: Boolean = false
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val rootView = inflater.inflate(R.layout.fragment_service_state, container, false)
         buttonSwitchMode = rootView.findViewById(R.id.switchMode) as Button
+        buttonSwitchBackground = rootView.findViewById(R.id.switchBackground) as Button
         buttonLogin = rootView.findViewById(R.id.loginService)
         buttonSlm = rootView.findViewById(R.id.slmService)
         header = rootView.findViewById(R.id.serviceStateText)
+        layout = rootView.findViewById(R.id.serviceBackground)
         buttonSwitchMode.setOnClickListener {
             //TODO: too dirty, fix it
             buttonLogin.rotationX += 180f
             buttonSlm.rotationX += 180f
             header.rotationX += 180f
+        }
+        buttonSwitchBackground.setOnClickListener {
+            if (isBlack) {
+                layout.setBackgroundColor(Color.WHITE)
+                isBlack = false
+            } else {
+                layout.setBackgroundColor(Color.BLACK)
+                isBlack = true
+            }
         }
         return rootView
     }
